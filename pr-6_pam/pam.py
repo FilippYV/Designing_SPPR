faba1eacf75384ecdfbd52846f61c01e41257eb8
Filisss_911 <102216221+FilippYV@users.noreply.github.com>
2023-04-27 19:01:02 +0300

    Pr_1 complete docx need modify Pr_2 complete Pr_3 complete Pr_4 complete Pr_5 complete Pr_6 complete

--- pr-6_pam/pam.py
+++ pr-6_pam/pam.py
@@ -68,120 +68,7 @@ def graph_elbow_long(mass_elbow_length):
     plt.close(fig)
 
 
-def elbow_method(mass):
-    color = ['#000000', '#808080', '#FF00FF', '#32CD32',
-             '#FFFF00', '#808000', '#00FF00', '#008000', '#0000FF']
-    minimum = 1
-    maximum = 5
-    mass_elbow_length = []
-    start_point = random.randint(0, len(mass) - 1)
-    for k in range(minimum, maximum + 1):
-        central_points = [start_point]  # начальная точка
-        if k != 1:  # для метода локтя
-            for n in range(k - 1):
-                minimum = [0, 0]
-                for i, element in enumerate(mass):
-                    if i not in central_points:
-                        long = 0
-                        for j in central_points:
-                            long += math.sqrt((element[0] - mass[j][0]) ** 2 + (element[1] - mass[j][1]) ** 2)
-                        if long > minimum[0]:
-                            minimum = [long, i]
-                central_points.append(minimum[1])
-                print(minimum)
-        graph_for_elbow_method(mass, central_points, k)
-        mass_to_clusters = []
-        for i, element in enumerate(mass):
-            minimum = [1000000, 0]
-            if i not in central_points:
-                for index, j in enumerate(central_points):
-                    long = math.sqrt((element[0] - mass[j][0]) ** 2 + (element[1] - mass[j][1]) ** 2)
-                    if long < minimum[0]:
-                        minimum = [long, index]
-                mass_to_clusters.append(minimum[1])
-            else:
-                for index, j in enumerate(central_points):
-                    if element[0] == mass[j][0] and element[1] == mass[j][1]:
-                        mass_to_clusters.append(index)
-        # выбор центральный точки
-        for class_name in range(len(central_points)):
-            mass_for_relocation = []
-            for index, j in enumerate(mass):
-                if mass_to_clusters[index] == class_name:
-                    mass_for_relocation.append(index)
-            minimum = [1000000, 0]
-            for index_i, i in enumerate(mass_for_relocation):
-                long = 0
-                for index_j, j in enumerate(mass_for_relocation):
-                    if index_j != index_i:
-                        long += math.sqrt((mass[i][0] - mass[j][0]) ** 2 + (mass[i][1] - mass[j][1]) ** 2)
-                if long < minimum[0]:
-                    minimum = [long, i]
-            central_points[class_name] = minimum[1]
-        # for a in range(3):
-        #     for class_name in range(len(central_points)):
-        #         mass_for_relocation = []
-        #         for index, j in enumerate(mass):
-        #             if mass_to_clusters[index] == class_name:
-        #                 mass_for_relocation.append(index)
-        #         minimum = [1000000, 0]
-        #         for index_i, i in enumerate(mass_for_relocation):
-        #             long = 0
-        #             for index_j, j in enumerate(mass_for_relocation):
-        #                 if index_j != index_i:
-        #                     long += math.sqrt((mass[i][0] - mass[j][0]) ** 2 + (mass[i][1] - mass[j][1]) ** 2)
-        #             if long < minimum[0]:
-        #                 minimum = [long, i]
-        #         central_points[class_name] = minimum[1]
-        #     mass_to_clusters = []
-        #     for i, element in enumerate(mass):
-        #         minimum = [1000000, 0]
-        #         if i not in central_points:
-        #             for index, j in enumerate(central_points):
-        #                 long = math.sqrt((element[0] - mass[j][0]) ** 2 + (element[1] - mass[j][1]) ** 2)
-        #                 if long < minimum[0]:
-        #                     minimum = [long, index]
-        #             mass_to_clusters.append(minimum[1])
-        #         else:
-        #             for index, j in enumerate(central_points):
-        #                 if element[0] == mass[j][0] and element[1] == mass[j][1]:
-        #                     mass_to_clusters.append(index)
-        graph_for_elbow_method_cluster(mass, central_points, k, color, mass_to_clusters)
-        elbow_length = 0
-        for class_name in range(len(central_points)):
-            mass_for_relocation = []
-            for index, j in enumerate(mass):
-                if mass_to_clusters[index] == class_name:
-                    mass_for_relocation.append(index)
-            index_i = central_points[class_name]
-            for index_j, j in enumerate(mass_for_relocation):
-                if mass_to_clusters[index_j] == class_name:
-                    elbow_length += math.sqrt(
-                        (mass[index_i][0] - mass[j][0]) ** 2 + (mass[index_i][1] - mass[j][1]) ** 2)
-        mass_elbow_length.append([elbow_length])
-        graph_elbow_long(mass_elbow_length)
-
-
-def centroid(mass):
-    color = ['#000000', '#808080', '#FF00FF', '#32CD32',
-             '#FFFF00', '#808000', '#00FF00', '#008000', '#0000FF']
-    start_point = random.randint(0, len(mass) - 1)
-    central_points = [start_point]  # начальная точка
-    k = int(input('Введите число кластеров: '))
-    if k != 1:  # для метода локтя
-        for n in range(k - 1):
-            minimum = [0, 0]
-            for i, element in enumerate(mass):
-                if i not in central_points:
-                    long = 0
-                    for j in central_points:
-                        long += math.sqrt((element[0] - mass[j][0]) ** 2 + (element[1] - mass[j][1]) ** 2)
-                    if long > minimum[0]:
-                        minimum = [long, i]
-            central_points.append(minimum[1])
-            print(minimum)
-    graph_for_elbow_method(mass, central_points, k)
-    mass_to_clusters = []
+def distribution_of_points_by_clusters(mass_to_clusters, mass, central_points):  # распределение точек по кластерам
     for i, element in enumerate(mass):
         minimum = [1000000, 0]
         if i not in central_points:
@@ -194,6 +81,87 @@ def centroid(mass):
             for index, j in enumerate(central_points):
                 if element[0] == mass[j][0] and element[1] == mass[j][1]:
                     mass_to_clusters.append(index)
+
+
+def redefining_cluster_centers(central_points, mass, mass_to_clusters):  # переопределение центров
+    for class_name in range(len(central_points)):
+        mass_for_relocation = []
+        for index, j in enumerate(mass):
+            if mass_to_clusters[index] == class_name:
+                mass_for_relocation.append(index)
+        minimum = [1000000, 0]
+        for index_i, i in enumerate(mass_for_relocation):
+            long = 0
+            for index_j, j in enumerate(mass_for_relocation):
+                if index_j != index_i:
+                    long += math.sqrt((mass[i][0] - mass[j][0]) ** 2 + (mass[i][1] - mass[j][1]) ** 2)
+            if long < minimum[0]:
+                minimum = [long, i]
+        central_points[class_name] = minimum[1]
+
+
+def calculation_of_long_elbows(central_points, mass, mass_to_clusters):
+    elbow_length = 0
+    for class_name in range(len(central_points)):
+        mass_for_relocation = []
+        for index, j in enumerate(mass):
+            if mass_to_clusters[index] == class_name:
+                mass_for_relocation.append(index)
+        index_i = central_points[class_name]
+        for index_j, j in enumerate(mass_for_relocation):
+            if mass_to_clusters[index_j] == class_name:
+                elbow_length += math.sqrt(
+                    (mass[index_i][0] - mass[j][0]) ** 2 + (mass[index_i][1] - mass[j][1]) ** 2)
+    return elbow_length
+
+
+def elbow_method(mass):
+    color = ['#000000', '#808080', '#FF00FF', '#32CD32',
+             '#FFFF00', '#808000', '#00FF00', '#008000', '#0000FF']
+    minimum = 1
+    maximum = 5
+    mass_elbow_length = []
+    start_point = random.randint(0, len(mass) - 1)
+    for k in range(minimum, maximum + 1):
+        central_points = [start_point]  # начальная точка
+        cluster_center_definitions(k, central_points, mass)  # опеределения центра кластеров
+        graph_for_elbow_method(mass, central_points, k)
+        mass_to_clusters = []
+        distribution_of_points_by_clusters(mass_to_clusters, mass, central_points)  # распределение точек по кластерам
+        redefining_cluster_centers(central_points, mass, mass_to_clusters)  # переопределение центров
+        graph_for_elbow_method_cluster(mass, central_points, k, color, mass_to_clusters)
+        elbow_length = calculation_of_long_elbows(central_points, mass, mass_to_clusters)
+        mass_elbow_length.append(elbow_length)
+        graph_elbow_long(mass_elbow_length)
+    for index, i in enumerate(mass_elbow_length):
+        print(f'Если количество элементов = {index + 1}, то\n'
+              f'Евклидово расстояние по правилу локтя = {i}\n')
+
+
+def cluster_center_definitions(k, central_points, mass):  # опеределения центра кластеров
+    if k != 1:  # для метода локтя
+        for n in range(k - 1):
+            minimum = [0, 0]
+            for i, element in enumerate(mass):
+                if i not in central_points:
+                    long = 0
+                    for j in central_points:
+                        long += math.sqrt((element[0] - mass[j][0]) ** 2 + (element[1] - mass[j][1]) ** 2)
+                    if long > minimum[0]:
+                        minimum = [long, i]
+            central_points.append(minimum[1])
+
+
+def centroid(mass):
+    color = ['#000000', '#808080', '#FF00FF', '#32CD32',
+             '#FFFF00', '#808000', '#00FF00', '#008000', '#0000FF']
+    start_point = random.randint(0, len(mass) - 1)
+    central_points = [start_point]  # начальная точка
+    k = int(input('Введите число кластеров: '))
+    cluster_center_definitions(k, central_points, mass)
+    graph_for_elbow_method(mass, central_points, k)
+    mass_to_clusters = []
+    distribution_of_points_by_clusters(mass_to_clusters, mass, central_points)
     for a in range(3):
         for class_name in range(len(central_points)):
             mass_for_relocation = []
