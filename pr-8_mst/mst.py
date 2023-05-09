e6c9e3761b08cb67414d506e2379d333e2ed05a4
Filisss_911 <102216221+FilippYV@users.noreply.github.com>
2023-05-09 22:47:55 +0300

    Pr_1 complete docx need modify Pr_2 complete Pr_3 complete Pr_4 complete Pr_5 complete Pr_6 complete

--- pr-8_mst/mst.py
+++ pr-8_mst/mst.py
@@ -1,5 +1,24 @@
-import math
+import random
+
+
+def generate_start_graph(count):
+    graph = []
+    for i in range(count - 1):
+        for j in range(i + 1, count):
+            graph.append([i, j, random.randint(10, 50)])
+    print('Начальный граф:')
+    for i in graph:
+        print(f'{i[0]}, {i[1]}, {i[2]}')
+    print()
+    return graph
+
+
+def output_start_graph(graph):
+    pass
 
 
 if __name__ == '__main__':
-    count_cytis = 5
+    count_city = 5
+    graph = generate_start_graph(count=count_city)
+    output_start_graph(graph)
+
